181445c26bafc7ee1aa21bdb5a6d02ccd5c09597
crazyassasin <kocherprateek2001@gmail.com>
2019-09-17 14:38:03 +0530

    2nd commit

---
+++ vigyaanCR/testflask/simple_request.py
@@ -0,0 +1,23 @@
+import requests 
+import sys 
+  
+URL = "http://localhost:5000/predict"
+  
+# provide image name as command line argument 
+IMAGE_PATH = sys.argv[1]  
+  
+image = open(IMAGE_PATH, "rb").read() 
+payload = {"image": image} 
+  
+# make request to the API 
+request = requests.post(URL, files = payload).json() 
+  
+if request["success"]: 
+    # Print formatted Result 
+    print("% s % 15s % s"%("Rank", "Label", "Probability")) 
+    for (i, result) in enumerate(request["predictions"]): 
+        print("% d.    % 17s %.4f"%(i + 1, result["label"], 
+            result["probability"])) 
+  
+else: 
+    print("Request failed") 
